f750fd858b15d76638af397770081f63c46fac1e
William_Kuang <fuwa165@126.com>
2026-05-19 18:02:49 +0800

    chore: 版本号升级至 v1.0.2

--- PigeonNest/app/src/main/java/com/pigeonnest/data/file/BackupManager.kt
+++ PigeonNest/app/src/main/java/com/pigeonnest/data/file/BackupManager.kt
@@ -80,7 +80,7 @@ class BackupManager @Inject constructor(
             val backupData = BackupData(
                 backup_version = BACKUP_VERSION,
                 backup_date = System.currentTimeMillis(),
-                app_version = "1.0.1",
+                app_version = "1.0.2",
                 data = BackupContent(
                     lofts = lofts,
                     pigeons = pigeons,
